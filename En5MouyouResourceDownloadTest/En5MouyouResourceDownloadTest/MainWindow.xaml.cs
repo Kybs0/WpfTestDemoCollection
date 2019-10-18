920f1090996f2203a6df55f183b835b971ec4d3a
余冬 <yudong@cvte.com>
2019-10-18 10:26:27 +0800

    上传demos

---
+++ En5MouyouResourceDownloadTest/En5MouyouResourceDownloadTest/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace En5MouyouResourceDownloadTest
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DownloadUrlTextBox.Text =
+                "http://static.cvte.com/file/myou/uploads/in_app_update/7dba5d0bcfcb408be66e1e4b6a52eeb54a34c900/59a392c0d57ed33333dd281c/59a39-0.0.0.10.zip";
+            DownloadLogTextBox.Text +=
+                "古诗词资源：http://static.cvte.com/file/myou/uploads/in_app_update/7dba5d0bcfcb408be66e1e4b6a52eeb54a34c900/59a392c0d57ed33333dd281c/59a39-0.0.0.10.zip\r\n";
+            DownloadLogTextBox.Text +=
+                "英汉字典资源：http://static.cvte.com/file/myou/uploads/android_rom/7dba5d0bcfcb408be66e1e4b6a52eeb54a34c900/English_Dict.0.0.7-ba204.zip\r\n";
+
+        }
+
+        private async void DownloadButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var text = DownloadUrlTextBox.Text;
+            if (!string.IsNullOrWhiteSpace(text))
+            {
+                var downloadUrl = text;
+
+                try
+                {
+                    DownloadLogTextBox.Text += $"正在下载{downloadUrl}\r\n";
+                    var filePath=await Task.Run(() =>
+                    {
+                        WebResourceDownloadHelper.Download(downloadUrl, out var downloadedPath);
+                        return downloadedPath;
+                    });
+                    DownloadLogTextBox.Text += $"下载完成，文件保存路径：{filePath}\r\n";
+                }
+                catch (Exception exception)
+                {
+                    DownloadLogTextBox.Text += $"下载失败：{exception.Message}\r\n";
+                }
+            }
+        }
+    }
+}
---
+++ En5MouyouResourceDownloadTest/En5MouyouResourceDownloadTest/WebResourceDownloadHelper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace En5MouyouResourceDownloadTest
+{
+    public class WebResourceDownloadHelper
+    {
+        public static bool Download(string resourceUri, out string downloadPath)
+        {
+            downloadPath = string.Empty;
+            if (string.IsNullOrWhiteSpace(resourceUri))
+            {
+                return false;
+            }
+            try
+            {
+                //"http://ydschool-online.nos.netease.com/account_v0205.mp3"
+                WebRequest request = WebRequest.Create(resourceUri);
+                WebResponse response = request.GetResponse();
+                using (Stream reader = response.GetResponseStream())
+                {
+                    var userDownloadFolder = GetUserDownloadFolder();
+                    downloadPath = Path.Combine(userDownloadFolder, $"{DateTime.Now.Ticks}-{Path.GetFileName(resourceUri)}");
+                    if (File.Exists(downloadPath))
+                    {
+                        File.Delete(downloadPath);
+                    }
+                    using (FileStream writer = new FileStream(downloadPath, FileMode.OpenOrCreate, FileAccess.Write))
+                    {
+                        byte[] buff = new byte[512];
+                        int c = 0;                                           //实际读取的字节数   
+                        while ((c = reader.Read(buff, 0, buff.Length)) > 0)
+                        {
+                            writer.Write(buff, 0, c);
+                        }
+                        response.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            //下载成功
+            return true;
+        }
+
+        private static string GetUserDownloadFolder()
+        {
+            string strDesktopPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            return strDesktopPath;
+        }
+    }
+}
