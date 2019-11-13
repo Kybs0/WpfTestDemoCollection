d5b15a7cc60872ee0a98e358a814f8ca95ee7bc1
余冬 <yudong@cvte.com>
2019-11-13 18:17:50 +0800

    异步队列：只保留最后一个

--- MultiTasksOnlyKeepLastDemo/MultiTasksOnlyKeepLastDemo/MainWindow.xaml.cs
+++ MultiTasksOnlyKeepLastDemo/MultiTasksOnlyKeepLastDemo/MainWindow.xaml.cs
@@ -1,7 +1,13 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Permissions;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +26,40 @@ namespace MultiTasksOnlyKeepLastDemo
     /// </summary>
     public partial class MainWindow : Window
     {
+        private AsyncTaskQueue _asyncTaskQueue;
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private async void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            using (var asyncTaskQueue = new AsyncTaskQueue
+            {
+                AutoCancelPreviousTask = true,
+                UseSingleThread = true
+            })
+            {
+                // 快速启动20个任务
+                for (var i = 1; i < 20; i++)
+                {
+                    await Task.Delay(TimeSpan.FromMilliseconds(1));
+                    Test(asyncTaskQueue, i);
+                }
+            }
+        }
+
+        public static async void Test(AsyncTaskQueue taskQueue, int num)
+        {
+            var result = await taskQueue.ExecuteAsync(async () =>
+            {
+                // 长时间耗时任务
+                await Task.Delay(TimeSpan.FromSeconds(5));
+                Debug.WriteLine("输入:" + num);
+                return num * 100;
+            });
+            Debug.WriteLine($"{num}输出的:" + result);
+        }
+
     }
 }
